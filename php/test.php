e550eb48fac4632527d7305277f005c32e8d3503
David <david.vrckovic.dv@gmail.com>
2024-05-02 22:30:35 +0200

    Test random sequence generation

--- php/test.php
+++ php/test.php
@@ -11,4 +11,10 @@ echo "<br>Encrypted pass: " . $encrypted_password;
 echo "<br><br>Current time: " . date("Y-m-d H:i:s");
 echo "<br><br>Current time: " . date("d F Y \a\\t H:i:s");
 
+
+
+echo "<br><br>Random int: " . random_int(1, pow(2, 48));
+echo "<br><br>Random bytes: " . random_bytes(32);
+echo "<br><br>Random bytes: " . bin2hex(random_bytes(32));
+
 ?>
