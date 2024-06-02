4561beda3c554038dbd72fc22732b499e9aa2dc2
David <david.vrckovic.dv@gmail.com>
2024-06-02 22:03:32 +0200

    Export of the latest DB

--- mathmatters.sql
+++ mathmatters.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 27, 2024 at 05:41 PM
+-- Generation Time: Jun 02, 2024 at 10:02 PM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.10
 
@@ -28,13 +28,36 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `mm_users` (
-  `id` int(9) UNSIGNED NOT NULL,
-  `first_name` varchar(64) NOT NULL,
-  `last_name` varchar(64) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
+  `user_id` int(9) UNSIGNED NOT NULL,
+  `first_name` varchar(64) COLLATE utf8mb4_bin NOT NULL,
+  `last_name` varchar(64) COLLATE utf8mb4_bin NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_bin NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_bin NOT NULL,
   `reg_date` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `completed_tasks` mediumtext DEFAULT NULL
+  `completed_tasks` mediumtext COLLATE utf8mb4_bin DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+--
+-- Dumping data for table `mm_users`
+--
+
+INSERT INTO `mm_users` (`user_id`, `first_name`, `last_name`, `email`, `password`, `reg_date`, `completed_tasks`) VALUES
+(1, 'David', 'Vrčković', 'dvrckovi1@tvz.hr', '$SHA#8365d0ace8a114d750b5cc8f78be5372f3183de7f407a3268e6294fe1d33cd5c', '2024-04-21 22:32:16', NULL),
+(2, 'David', 'Vrčković', 'dvrckovi2@tvz.hr', '$SHA#8365d0ace8a114d750b5cc8f78be5372f3183de7f407a3268e6294fe1d33cd5c', '2024-04-30 22:29:36', NULL),
+(3, 'David', 'Vrc', 'davy.navy.3@gmail.com', '$SHA#533cd865c088ee083940b3c9bf2d456244842fd80bbe5fed6beb331895721153', '2024-05-05 17:19:23', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mm_user_sessions`
+--
+
+CREATE TABLE `mm_user_sessions` (
+  `session_id` int(9) UNSIGNED NOT NULL,
+  `user_id` int(9) UNSIGNED NOT NULL,
+  `session_token` varchar(255) COLLATE utf8mb4_bin NOT NULL,
+  `session_created_date` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `session_expire_date` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 --
@@ -45,7 +68,14 @@ CREATE TABLE `mm_users` (
 -- Indexes for table `mm_users`
 --
 ALTER TABLE `mm_users`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- Indexes for table `mm_user_sessions`
+--
+ALTER TABLE `mm_user_sessions`
+  ADD PRIMARY KEY (`session_id`),
+  ADD KEY `user_id` (`user_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -55,7 +85,23 @@ ALTER TABLE `mm_users`
 -- AUTO_INCREMENT for table `mm_users`
 --
 ALTER TABLE `mm_users`
-  MODIFY `id` int(9) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `user_id` int(9) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `mm_user_sessions`
+--
+ALTER TABLE `mm_user_sessions`
+  MODIFY `session_id` int(9) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `mm_user_sessions`
+--
+ALTER TABLE `mm_user_sessions`
+  ADD CONSTRAINT `mm_user_sessions_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `mm_users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
